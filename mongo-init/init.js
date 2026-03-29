6c8abd5f17c3f12f69e1acb2d2426ffdaff5f38e
Alexandre Lemiere <lemiere.alexandre2015@gmail.com>
2026-03-29 11:22:53 +0200

    chore: adapte le mongo-init pour notre base crypto_portfolio

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -1,7 +1,10 @@
-db = db.getSiblingDB('myapp')
+// Script d'init joué au premier démarrage du container Mongo (volume vide).
+// Crée le user applicatif utilisé par le backend en dev local.
+// Si on change ces valeurs, il faut reset le volume : docker compose down -v
+db = db.getSiblingDB('crypto_portfolio')
 
 db.createUser({
-  user: 'stan',
-  pwd: 'stan',
-  roles: [{ role: 'dbOwner', db: 'myapp' }],
+  user: 'crypto_app',
+  pwd: 'dev_password_2026',
+  roles: [{ role: 'dbOwner', db: 'crypto_portfolio' }],
 })
