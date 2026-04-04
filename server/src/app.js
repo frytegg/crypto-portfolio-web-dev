a8afc72856a3ce8ac32b4281c81cdf23799079fe
Alexandre Lemiere <lemiere.alexandre2015@gmail.com>
2026-04-04 14:22:49 +0200

    tech: branche les routes holdings et portfolio dans app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 import Fastify from 'fastify'
 
 import config from './config.js'
+import holdingsRoutes from './holdings/holdings-routes.js'
 import envToLogger from './logger.js'
 import authPlugin from './plugins/auth.js'
+import corsPlugin from './plugins/cors.js'
 import mongoosePlugin from './plugins/mongoose.js'
+import portfolioRoutes from './portfolio/portfolio-routes.js'
 import rootRoutes from './rootRoute.js'
 import authRoutes from './users/auth-routes.js'
 import usersRoutes from './users/users-routes.js'
@@ -13,10 +16,13 @@ async function buildApp() {
     logger: envToLogger[config.env] ?? true,
   })
 
+  await fastify.register(corsPlugin)
   await fastify.register(authPlugin)
   await fastify.register(mongoosePlugin)
   fastify.register(authRoutes, { prefix: '/auth' })
   fastify.register(usersRoutes, { prefix: '/users' })
+  fastify.register(holdingsRoutes, { prefix: '/holdings' })
+  fastify.register(portfolioRoutes, { prefix: '/portfolio' })
   fastify.register(rootRoutes)
 
   return fastify
